efdb49cc739c594f10c68c0459f3dfdec68ee996
Rajesh Kumar <rajeshceg3@gmail.com>
2018-12-06 12:57:18 +0530

    Update vector_create_with_arrays.cpp

--- vector_create_with_arrays.cpp
+++ vector_create_with_arrays.cpp
@@ -5,8 +5,10 @@
 int main(){
     int arr[5] = { 1,2,3,4,5};
     std::vector<int> vec(arr, arr+5);
+    std::cout<< "Size of vector: "<<vec.size()<< std::endl;
     for ( auto i: vec){
         std::cout<< i <<"   ";
     }
     std::cin.get();
 }
+
