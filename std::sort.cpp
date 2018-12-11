7bf86cc333d81228b63f9cb2171f680776c8c5b1
Rajesh <rajeshceg3@gmail.com>
2018-12-11 17:45:58 +0530

    Create std::sort.cpp

---
+++ std::sort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <functional>
+#include <ctime>
+using namespace std;
+
+int main(){
+    vector<int> collection;
+    srand(time(nullptr));
+
+    // fill vector with 10 random numbers
+    for(int index = 0; index <10 ; ++index){
+        collection.emplace_back( std::rand());
+    }
+
+    cout << "10 random numbers"<< endl;
+    ostream_iterator<int> cout_it(cout, ": ");
+
+    for( const auto&i: collection){
+        *cout_it = i;
+    }
+
+    sort(collection.begin(),collection.end());
+    cout <<endl<< "After std::sort"<<endl;
+
+    copy(begin(collection),end(collection),ostream_iterator<int> (cout, ": "));
+}
