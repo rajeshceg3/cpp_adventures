6dbd1ee52f96dd100b0772471497052eb25ef0a9
Rajesh <rajeshceg3@gmail.com>
2018-12-17 12:20:24 +0530

    Update lambda_generic.cpp

--- lambda_generic.cpp
+++ lambda_generic.cpp
@@ -3,14 +3,9 @@
 using namespace std;
 
 int main(){
-    
+
     // Generic lambdas, lambdas that take auto as a parameter are supported from c++14 onwards
     function <void(double)> f = [](auto i)
-    {
-        cout<<i<<endl;
-    };
-
-    function <void(int)> g = [](auto i)
     {
         cout<<i<<endl;
     };
@@ -19,4 +14,7 @@ int main(){
     f(5);
     // invoke the lambda with double
     f(5.65);
+    // invoke the lambda with bool
+    f(true);
     }
+
