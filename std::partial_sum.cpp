e89c9feb8c1af15e6019988ef59783024b7546fb
Rajesh <rajeshceg3@gmail.com>
2018-12-18 13:00:11 +0530

    Create std::partial_sum.cpp

---
+++ std::partial_sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    std::vector<int> vec(10);
+    // generate a sequence
+    iota(begin(vec),end(vec),2);
+    cout<<"\nSequence generated\n";
+    move(begin(vec),end(vec), ostream_iterator<int> (cout," "));
+    partial_sum(begin(vec),end(vec),begin(vec));
+    cout<<"\nSequence generated after partial_sum \n";
+    move(begin(vec),end(vec), ostream_iterator<int> (cout," "));
+}
