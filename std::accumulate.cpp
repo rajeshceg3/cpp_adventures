84f120f25972228ce7b4fc02b13f5737f3ab2800
Rajesh <rajeshceg3@gmail.com>
2018-12-19 12:56:40 +0530

    Create std::accumulate.cpp

---
+++ std::accumulate.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <random>
+#include <ctime>
+#include <numeric>
+#include <iterator>
+using namespace std;
+
+int main(){
+    srand(time(nullptr));
+    std::vector<int> vec(10);
+    generate_n(begin(vec), 10, [](){
+        return rand()%20;
+    });
+    cout<<"Original sequence\n";
+    move(begin(vec), end(vec), ostream_iterator<int> (cout," "));
+    cout<<"\n";
+    cout<<"Sum = "<<accumulate(begin(vec),end(vec),0)<<"\n";
+
+    cout<<"Product of container elements "
+          "using multiplies functor= "<<accumulate(begin(vec),end(vec),1,multiplies<>())<<"\n";
+
+    cout<<"Accumulate with lambda = "<<accumulate(begin(vec),end(vec),0,[](auto x, auto y) -> decltype(auto)
+    {
+        return x+y+1;
+    })<<"\n";
+}
