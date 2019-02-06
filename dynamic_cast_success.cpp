e313eb1705d2d06e1f8a9f16db19ccd656eae6fc
Rajesh <rajeshceg3@gmail.com>
2019-02-06 19:02:20 +0530

    Update dynamic_cast_success.cpp

--- dynamic_cast_success.cpp
+++ dynamic_cast_success.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+// Base class 
 class Base{
 public:
      virtual void show();
@@ -8,6 +9,8 @@ public:
 void Base::show(){
     cout << "Base::show" << endl;
 }
+
+// Derived class 
 class Derived: public Base {
 public:
      void show();
@@ -16,9 +19,17 @@ void Derived::show(){
     cout<< "Derived::show"<<endl;
 }
 
-void printshow(Base* b){
-    Derived* dptr = dynamic_cast<Derived*> (b);
 
+// printshow method which takes a pointer to base 
+void printshow(Base* b){
+    // we know that, pointer that is passed is of derived type
+    // So, we inititate a dynamic cast, to check whether it is
+    // really the case, and whether it can be safely done
+    // As this is a downcast, base -> derived, this check is necessary
+    // upcast is always safe and guaranteed to succeed
+    Derived* dptr = dynamic_cast<Derived*> (b);
+    
+    // if resulting pointer is not null, then dynamic_cast is success
     if (NULL != dptr )
         dptr->show();
     else
@@ -29,7 +40,8 @@ void printshow(Base* b){
 int main(){
     Base* b = new Base();
     Derived *d = new Derived();
-    // Call printshow with base object
-    // with show method marked as virtual
+    // Call printshow with derived object
+    // Notice that, this call is succeding as base class show is a virtual
+    // function, else this will result in failure
     printshow(d);
 }
