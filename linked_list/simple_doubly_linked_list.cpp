87edcdd8a24c4f5166d866075c4a88e70bc97225
Rajesh <rajeshceg3@gmail.com>
2019-02-07 12:20:50 +0530

    Create simple_doubly_linked_list.cpp

---
+++ linked_list/simple_doubly_linked_list.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+	int data;
+	Node* prev;
+	Node* next;
+};
+
+
+Node* createNode(int data){
+	Node* temp = new Node();
+	temp->data = data;
+	temp->prev = NULL;
+	temp->next = NULL;
+    return temp;
+}
+
+int main()
+{
+	int arr[5] = {1,2,3,4,5};
+	Node* Node_arr[5] = {};
+	int Node_index = 0;
+	for (auto &a: arr){
+	Node_arr[Node_index++] = createNode(a);
+	}
+   
+  for(auto& i : Node_arr){
+      cout<<i->data << "\n";
+  }
+}
