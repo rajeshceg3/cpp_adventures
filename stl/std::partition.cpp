31979ee5487bda9026f4b2d7991f42c4dba72f66
Rajesh <rajeshceg3@gmail.com>
2019-02-26 17:16:04 +0530

    Create std::partition.cpp

---
+++ stl/std::partition.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// function template to print the contents of vector
+template <typename T> void printvec(const vector<T> &vec) {
+  for (const auto &i : vec)
+    cout << i << " ";
+  cout << "\n";
+}
+
+int main() {
+  srand(time(0));
+  vector<int> vec;
+  printvec(vec);
+  
+  // store 10 random elements in vec, max value is 99
+  generate_n(back_inserter(vec), 10, []() { return rand() % 100; });
+  printvec(vec);
+  
+  // std::parititon to move all even elements to front
+  // predicate can specify any condition for a match
+  auto partition_it = partition(
+      begin(vec), end(vec), [](auto element) { return (element % 2) == 0; });
+  printvec(vec);
+  cout << "Paritition point = " << *partition_it;
+}
