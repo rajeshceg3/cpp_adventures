f98c738e1a765d716a712a6fe48feac0332532f6
Rajesh <rajeshceg3@gmail.com>
2020-02-06 11:42:16 +0530

    Update clientserver.cpp

--- zmq/clientserver.cpp
+++ zmq/clientserver.cpp
@@ -3,25 +3,24 @@
 #include <unistd.h>
 #include "zmq.h"
 
-int main (int argc, char const *argv[]) {
+int main () {
   
   void* context = zmq_ctx_new();
   void* respond = zmq_socket(context, ZMQ_REP);
   zmq_bind(respond, "tcp://*:7070");
 
-  printf("Starting…\n");
-
-  for(;;) {
+  while(true)
+  {
     zmq_msg_t request;
     zmq_msg_init(&request);
     zmq_msg_recv(&request, respond, 0);
     printf("Received: hello\n");
     zmq_msg_close(&request);
-    sleep(1); // sleep one second
+    sleep(1); // sleep for a second
     
     zmq_msg_t reply;
-    zmq_msg_init_size(&reply, strlen("world"));
-    memcpy(zmq_msg_data(&reply), "world", 5);
+    zmq_msg_init_size(&reply, strlen("gutentag"));
+    memcpy(zmq_msg_data(&reply), "gutentag", 8);
     zmq_msg_send(&reply, respond, 0);
     zmq_msg_close(&reply);
   }
