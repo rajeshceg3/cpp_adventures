460cbd3e1d281b5d7c430f8fd03bbdd608f8ad5e
Rajesh <rajeshceg3@gmail.com>
2020-02-07 08:55:47 +0530

    Create stringBenchmark.cpp

---
+++ googleBenchmark/stringBenchmark.cpp
@@ -0,0 +1,24 @@
+#include <benchmark/benchmark.h>
+#include <vector>
+#include <string>
+using namespace std;
+
+static void stringOptimization(benchmark::State &s)
+{
+    int len = s.range(0);
+    vector<string> vec;
+    vec.reserve(100000);
+
+    for(auto _ : s)
+	{
+        for(int i = 0; i < 100000; i++)
+		{
+			// Construct string of specified length
+            string str(len, 's');
+            vec._back(str);
+        }
+    }
+}
+
+BENCHMARK(stringOptimization)->DenseRange(0,32)->Unit(benchmark::kMillisecond);
+BENCHMARK_MAIN();
