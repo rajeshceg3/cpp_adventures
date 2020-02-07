c7afb73ed4c92e512ca0bbff8faaa66bf6748197
Rajesh <rajeshceg3@gmail.com>
2020-02-07 09:32:30 +0530

    Create sampleClass.cpp

---
+++ makefile/sampleClass.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "sampleClass.h"
+
+using namespace std;
+
+void vector1::print()
+{
+    cout << "Start x: " << start_x << ", Start y: " << start_y << endl;
+    cout << "End x: " << end_x << ", End y: " << end_y << endl;
+}
