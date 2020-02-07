c3615cfa0891d5d970a27e03ec5042a3b26f78ed
Rajesh <rajeshceg3@gmail.com>
2020-02-07 09:34:17 +0530

    Create sampleClass.h

---
+++ makefile/sampleClass.h
@@ -0,0 +1,22 @@
+class vector1{
+public:
+    double start_x;
+    double start_y;
+    double end_x;
+    double end_y;
+
+    void print();
+};
+
+class point{
+public:
+    double x;
+    double y;
+};
+
+
+class vector2{
+public:
+    point start;
+    point end;
+};
