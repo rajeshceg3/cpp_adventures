9328137df38893dc7a778bb61886361a5ae3f7e0
Rajesh <rajeshceg3@gmail.com>
2020-02-20 10:22:51 +0530

    Create string_api.cpp

---
+++ cpp_basics/string_api.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string str("Hello World");
+
+    std::cout << str.front() << "\n";
+    std::cout << str.back() << "\n";
+    std::cout << str.at(1) << "\n";
+    std::cout << str.data() << "\n";
+	std::cout << str.size() << "\n";
+    std::cout << str.max_size() << "\n";
+    std::cout << str.empty() << "\n";
+	std::cout << str.substr(0, 5) << "\n";
+	
+    if (str.compare("Hello World") == 0) 
+        std::cout << "Hello World\n";
+    
+
+    std::cout << str.compare("Hello") << "\n";
+    std::cout << str.compare("World") << "\n";	
+	
+    str = "Hello this is a test of Hello World";
+	
+	// Returns the index of corresponding search results
+    std::cout << str.find("Hello") << "\n";
+    std::cout << str.rfind("Hello") << "\n";
+    std::cout << str.find_first_of("Hello") << "\n";
+    std::cout << str.find_last_of("Hello") << "\n";
+    std::cout << str.find_first_not_of("Hello") << "\n";
+    std::cout << str.find_last_not_of("Hello") << "\n";	
+	
+	return 0;
+}
