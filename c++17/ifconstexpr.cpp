4e2d68e982dd1df461e26e8addbc29da1105a1db
Rajesh <rajeshceg3@gmail.com>
2020-02-20 12:10:08 +0530

    Update ifconstexpr.cpp

--- c++17/ifconstexpr.cpp
+++ c++17/ifconstexpr.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
-
+/*
+if constexpr can replace SFINAE in many cases
+ie when the condition under if constexpr evaluates
+to false, the branch is totally discared, ie not
+even considered for compilation
+*/
 int main()
 {
     if constexpr (constexpr int i = 100; i > 0) 
-        std::cout<<"Hola\n";    
+        std::cout<<"Hola\n"; 
+    else
+        std::cout<<"This branch isn't even gonna compile\n";
 }
