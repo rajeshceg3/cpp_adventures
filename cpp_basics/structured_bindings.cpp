9c722d604e8d8a8f7027944237317b2c6e831ebc
Rajesh <rajeshceg3@gmail.com>
2020-02-20 10:14:40 +0530

    Update structured_bindings.cpp

--- cpp_basics/structured_bindings.cpp
+++ cpp_basics/structured_bindings.cpp
@@ -6,8 +6,24 @@ std::pair<std::string, int> pair_generator()
     return {"Year is: ", 2050};
 }
 
-int main(void)
+struct Sample
+{
+    string s_msg;
+    int s_year;
+};
+
+Sample struct_generator()
+{
+    return {"Year is: ", 2050};
+}
+
+int main()
 {
     auto [msg, year] = pair_generator();
-    std::cout<<msg<<year <<"\n";
+    auto a_pair = pair_generator();
+    auto [s_msg, s_year] = struct_generator();
+    
+    std::cout<< msg << year <<"\n";
+    std::cout<< std::get <0>(a_pair) << std::get<1>(a_pair) <<"\n";
+    std::cout << s_msg << s_year << '\n';
 }
