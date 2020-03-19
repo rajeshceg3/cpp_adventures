e4dd306994fbd59c08100eae13a8ba6e1414c9f4
Rajesh <rajeshceg3@gmail.com>
2020-03-19 15:00:56 +0530

    Create regex_replace.cpp

---
+++ regex/regex_replace.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <regex>
+
+int main()
+{
+	// Usage of capture groups along with regex_replace
+   std::string text = "today is 31.12.2019!";
+   std::regex rex( R"((\d{1,2})(\.|-|/)(\d{1,2})(\.|-|/)(\d{4}))" );
+   std::string result = std::regex_replace(text.data(), rex, R"($5-$3-$1)");	
+   std::cout<<result<<"\n";
+}
